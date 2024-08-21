5b3a5aa19301041709d3007839fc85738540b172
Asanobm <asanobm@outlook.com>
2024-08-21 13:24:54 +0900

    chore: Add spring-boot-starter-actuator dependency
    This commit adds the `spring-boot-starter-actuator` dependency to the project's `build.gradle.kts` file. This dependency will enable the actuator endpoints in the Spring Boot application, providing useful information and monitoring capabilities.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
 
   compileOnly("org.projectlombok:lombok")
 
